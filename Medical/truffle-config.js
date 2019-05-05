350cbff7ae3b60846cefe8add533c01a844608f8
assesspool <assesspool@gmail.com>
2019-05-05 17:29:06 -0400

    Added changes to deploy to ropsten * disabled simulation dry-run which was causing errors

--- Medical/truffle-config.js
+++ Medical/truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-                               //  ./client/node_modules/truffle-hdwallet-provider
-var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var HDWalletProvider = require("./client/node_modules/truffle-hdwallet-provider");
 var infura_apikey = "528eb380a5044c07a934acf2ca7773e2"; // Not pushing my key
 var mnemonic = "twin anchor library borrow output powder track coach tribe culture pear viable";  // Not pushing my seed words
 
@@ -16,10 +16,13 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      host: "127.0.0.1",
-      port: 8545,
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+infura_apikey),
+    //  host: "127.0.0.1",
+    //  port: 8545,
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/"+infura_apikey)
+      },
       network_id: 3,
+      skipDryRun: true, // I don't know why I had to do this but would not deploy until I disabled simulation run
       gas:4700000
     }
   }
